643528ecde87962eb17ba712c95fffeff29f586c
Mosanna-Jalal <74869986+Mosanna-Jalal@users.noreply.github.com>
2021-11-30 22:24:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var aap=0,bjp=0,congress=0;
+
+function handleBJP(){
+     bjp=bjp+1;
+     console.log("bjp="+bjp);
+     let x=document.getElementById("x");
+    x.innerText="BJP votes="+bjp;
+}
+
+function handleCongress(){
+    congress=congress+1;
+    console.log("congress="+congress);
+    let x=document.getElementById("y");
+    x.innerText="INC votes="+congress;
+    
+}
+
+function handleAAP(){
+    aap=aap+1;
+    console.log("aap="+aap);
+    let x=document.getElementById("z");
+    x.innerText="AAP votes="+aap;
+}
+function handleResult(){
+
+    var final="";
+if(aap>congress&&aap>bjp){
+    final="AAP wins";
+}
+else if(bjp>congress&&bjp>aap){
+    final="BJP wins";
+}
+else{
+    final="INC wins";
+}
+
+let result=document.getElementById("res");
+result.style.fontSize="50px";
+result.innerText=final;
+}
